71aaa321bfeb050e63208b7e03a6458652cefc0d
Maxim Faleev <m.faleev@innopolis.university>
2026-08-08 19:45:26 +0300

    Add shared PII item model

---
+++ pii_item.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+
+PIIValues = Tuple[str, ...]
+
+
+@dataclass(frozen=True)
+class PIIItem:
+    first_name: PIIValues = ()
+    middle_name: PIIValues = ()
+    last_name: PIIValues = ()
+    age: PIIValues = ()
+    birthdate: PIIValues = ()
+    phone: PIIValues = ()
+    email: PIIValues = ()
+    social_network_identifier: PIIValues = ()
+    location: PIIValues = ()
+    ssn: PIIValues = ()
